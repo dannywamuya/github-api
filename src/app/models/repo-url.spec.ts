18dfb447c7a8f15f38a718cc73391af09ff46e7f
dannywamuya <dwamuya@gmail.com>
2020-10-05 09:48:17 +0300

    Add user and repo URL classes

---
+++ src/app/models/repo-url.spec.ts
@@ -0,0 +1,7 @@
+import { RepoURL } from './repo-url';
+
+describe('RepoURL', () => {
+  it('should create an instance', () => {
+    expect(new RepoURL()).toBeTruthy();
+  });
+});
---
+++ src/app/models/user-url.spec.ts
@@ -0,0 +1,7 @@
+import { UserURL } from './user-url';
+
+describe('UserURL', () => {
+  it('should create an instance', () => {
+    expect(new UserURL()).toBeTruthy();
+  });
+});
