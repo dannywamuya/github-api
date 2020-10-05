1a1364c1260a4a71a8ea5e8a874759736f0bf2af
dannywamuya <dwamuya@gmail.com>
2020-10-05 09:58:42 +0300

    Add repositories and users component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoriesComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
